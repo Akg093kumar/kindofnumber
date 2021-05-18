7e2b8c7260f8a6bdea0194186e5a4242fbe8e93e
AsishKumarGouda <84058697+Akg093kumar@users.noreply.github.com>
2021-05-18 09:13:21 +0530

    update Armstrong.py
    New comment lines are added for increasing readability

--- Armstrong_num.py
+++ Armstrong_num.py
@@ -2,21 +2,24 @@
 
 num = int(input("Enter an integer: "))
 tmp = num
-s = 0
+sum = 0
+
 c1 = str(num)   #convert to string to find length 
-c = len(c1)
+c = len(c1)     #counts no of digits in the entered number
 c2 = int(c1)    #re-convert to integer for further calculations
-r = 0
+
+r = 0           #remainder
 
 while tmp != 0:
     r = tmp % 10
-    s = s + pow(r,c)
+    sum = sum + pow(r,c)
     tmp = tmp // 10
 
-if(s == num):
+if(sum == num):
     print(num,"is  an Armstrong Number.")
 else:
     print(num,"is not an Armstrong Number.")
 
 
 
+#code is contributed by Asish Kumar Gouda
